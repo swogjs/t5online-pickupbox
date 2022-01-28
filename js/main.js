45a8d173ff273b43b240ea7adcf3ef061c78c50e
PARKKYUTAE <satcop@naver.com>
2022-01-29 04:22:07 +0900

    메인작업

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+
+
+    $('.bind-add-order').on('click', function() {
+
+      
+        $('.popup-order').show();
+        
+
+    });
+
+    $('.bind-button-close').on('click', function() {
+
+        $('.popup-order').hide();
+
+
+    });
+
+
+});
